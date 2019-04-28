d9af99057d35035d9ca23bd948fdb905db947571
amitbb93 <amitbb93@gmail.com>
2019-04-28 14:25:09 +0300

    first commit

--- calculate.cpp
+++ calculate.cpp
@@ -1,19 +1,21 @@
 #include "calculate.hpp"
 #include <string>
-string bullpgia::calculateBullAndPgia(string choose, string guess)
-{
-    uint bull = 0;
-	uint pgia = 0;
-    for(int i=0; i<choose.size();i++)
-    {
-        if(choose[i]==guess[i])
-            bull++;
-		else{
-			for(int j=0; j<guess.size(); j++){
-				if(choose[i]==guess[j] && i!=j)
-					pgia++;
+namespace bullpgia{
+	string calculateBullAndPgia(string choose, string guess)
+	{
+		uint bull = 0;
+		uint pgia = 0;
+		for(int i=0; i<choose.size();i++)
+		{
+			if(choose[i]==guess[i])
+				bull++;
+			else{
+				for(int j=0; j<guess.size(); j++){
+					if(choose[i]==guess[j] && i!=j)
+						pgia++;
+				}
 			}
 		}
-    }
-	return to_string(bull)+","+to_string(pgia);
+		return to_string(bull)+","+to_string(pgia);
+	}
 }
