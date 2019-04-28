61eb2486ebc6c4d5b73c7f1696b20b86fc1edbe4
amitbb93 <amitbb93@gmail.com>
2019-04-28 14:29:30 +0300

    first commit

--- Test.cpp
+++ Test.cpp
@@ -9,14 +9,23 @@
 #include <iostream>
 using namespace std;
 
-#include "play.hpp"
+/*#include "play.hpp"
 #include "calculate.hpp"
 #include "DummyChoosers.hpp"
 #include "DummyGuessers.hpp"
 #include "SmartGuesser.hpp"
 #include "badkan.hpp"
-#define COMMA ,
+#define COMMA ,*/
 
+#include "play.hpp"
+#include "DummyChoosers.hpp"
+#include "DummyGuessers.hpp"
+#include "SmartGuesser.hpp"
+#include "badkan.hpp"
+#include <random>
+#include <chrono>
+#include "calculate.hpp"
+#define COMMA ,
 using namespace bullpgia;
 
 int main() {
